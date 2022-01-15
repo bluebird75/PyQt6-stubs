917b5a2bc59e46aae8a2fd7f5106a82a1d0f6ea7
Philippe FREMY <philippe.fremy@idemia.com>
2022-01-15 16:37:55 +0100

    Fix pyqtSlot by marking it as a proper decorator

--- tests/qtwindowtype.py
+++ tests/qtwindowtype.py
@@ -40,7 +40,7 @@ def test_on_one_flag_class() -> None:
 	assert_type_of_value_oneFlag(oneFlagValue1 | oneFlagValue2)
 	assert_type_of_value_oneFlag(oneFlagValue1 & oneFlagValue2)
 	assert_type_of_value_oneFlag(oneFlagValue1 ^ oneFlagValue2)
-	assert_type_of_value_oneFlag(~oneFlagValue1)
+	assert_type_of_value_oneFlag(~oneFlagValue1)				# type: ignore
 	assert_type_of_value_int(oneFlagValue1 + oneFlagValue2)
 	assert_type_of_value_int(oneFlagValue1 - oneFlagValue2)
 	assert_type_of_value_int(oneFlagValue1 * oneFlagValue2)
